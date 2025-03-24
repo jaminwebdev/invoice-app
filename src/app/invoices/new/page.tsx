09e82d99b11840245a13d0f2e7c2669600188a76
Jamin <jaminwebdev@gmail.com>
2025-03-24 11:09:25 -0500

    new invoice page

---
+++ src/app/invoices/new/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { type SyntheticEvent, useState } from 'react';
+
+// import Form from 'next/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+
+import SubmitButton from '@/components/SubmitButton';
+import Container from '@/components/Container';
+
+function CreateInvoice() {
+  const [state, setState] = useState('ready');
+  const [error, setError] = useState<string>();
+
+  async function handleOnSubmit(event: SyntheticEvent) {
+    // Submission disabled for demo
+    event.preventDefault();
+    setError('Submission disabled for demo.');
+    return;
+
+    if (state === 'pending') {
+      event.preventDefault();
+      return;
+    }
+    setState('pending');
+  }
+
+  return (
+    <main className="h-full min-h-screen">
+      <Container>
+        {error && (
+          <p className="bg-red-100 text-sm text-red-800 text-center px-3 py-2 rounded-lg mb-6">
+            {error}
+          </p>
+        )}
+        <div className="flex justify-between mb-6">
+          <h1 className="text-3xl font-semibold">Create Invoice</h1>
+        </div>
+
+        <form onSubmit={handleOnSubmit} className="grid gap-4 max-w-xs">
+          <div>
+            <Label htmlFor="name" className="block font-semibold text-sm mb-2">
+              Billing Name
+            </Label>
+            <Input id="name" name="name" type="text" />
+          </div>
+          <div>
+            <Label htmlFor="email" className="block font-semibold text-sm mb-2">
+              Billing Email
+            </Label>
+            <Input id="email" name="email" type="email" />
+          </div>
+          <div>
+            <Label htmlFor="value" className="block font-semibold text-sm mb-2">
+              Value
+            </Label>
+            <Input id="value" name="value" type="text" />
+          </div>
+          <div>
+            <Label
+              htmlFor="description"
+              className="block font-semibold text-sm mb-2"
+            >
+              Description
+            </Label>
+            <Textarea id="description" name="description" />
+          </div>
+          <div>
+            <SubmitButton />
+          </div>
+        </form>
+      </Container>
+    </main>
+  );
+}
+
+export default CreateInvoice;
