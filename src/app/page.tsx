66c339f86b1ef9158329814e3ee8fb41a2bf2197
Jamin <jaminwebdev@gmail.com>
2025-03-25 09:41:33 -0500

    homepage and error pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-6 justify-center items-center text-center h-full min-h-screen max-w-6xl mx-auto">
+    <main className="flex flex-col gap-6 justify-center items-center text-center h-full max-w-6xl mx-auto">
       <h1 className="text-5xl font-bold">Invoice Generator</h1>
       <Button asChild>
         <Link href="/dashboard">Sign In</Link>
