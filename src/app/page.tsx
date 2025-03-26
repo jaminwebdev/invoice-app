11d74f944b60ade6f191840a52e5ec285fad341d
Jamin <jaminwebdev@gmail.com>
2025-03-26 15:14:40 -0500

    redirect on homepage for authed users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  if (userId) redirect('/dashboard');
+
   return (
     <main className="flex flex-col gap-6 justify-center items-center text-center h-full max-w-6xl mx-auto">
       <h1 className="text-5xl font-bold">Invoice Generator</h1>
