1994f208a3dcc2e2b02f16561d7b24155ce8a814
Jamin <jaminwebdev@gmail.com>
2025-03-26 15:04:52 -0500

    new invoice page

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Form from 'next/form';
 import { type SyntheticEvent, useState } from 'react';
-
-// import Form from 'next/form';
+import { createAction } from '@/actions/actions';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
@@ -12,14 +12,8 @@ import Container from '@/components/Container';
 
 function CreateInvoice() {
   const [state, setState] = useState('ready');
-  const [error, setError] = useState<string>();
 
   async function handleOnSubmit(event: SyntheticEvent) {
-    // Submission disabled for demo
-    event.preventDefault();
-    setError('Submission disabled for demo.');
-    return;
-
     if (state === 'pending') {
       event.preventDefault();
       return;
@@ -28,49 +22,56 @@ function CreateInvoice() {
   }
 
   return (
-    <main className="h-full min-h-screen">
+    <main className="h-full">
       <Container>
-        {error && (
-          <p className="bg-red-100 text-sm text-red-800 text-center px-3 py-2 rounded-lg mb-6">
-            {error}
-          </p>
-        )}
-        <div className="flex justify-between mb-6">
+        <div className="max-w-3xl mx-auto flex flex-col gap-4 items-center mt-10">
           <h1 className="text-3xl font-semibold">Create Invoice</h1>
+          <Form
+            action={createAction}
+            onSubmit={handleOnSubmit}
+            className="flex flex-col gap-4 w-full"
+          >
+            <div>
+              <Label
+                htmlFor="name"
+                className="block font-semibold text-sm mb-2"
+              >
+                Billing Name
+              </Label>
+              <Input id="name" name="name" type="text" />
+            </div>
+            <div>
+              <Label
+                htmlFor="email"
+                className="block font-semibold text-sm mb-2"
+              >
+                Billing Email
+              </Label>
+              <Input id="email" name="email" type="email" />
+            </div>
+            <div>
+              <Label
+                htmlFor="value"
+                className="block font-semibold text-sm mb-2"
+              >
+                Value
+              </Label>
+              <Input id="value" name="value" type="text" />
+            </div>
+            <div>
+              <Label
+                htmlFor="description"
+                className="block font-semibold text-sm mb-2"
+              >
+                Description
+              </Label>
+              <Textarea id="description" name="description" />
+            </div>
+            <div>
+              <SubmitButton />
+            </div>
+          </Form>
         </div>
-
-        <form onSubmit={handleOnSubmit} className="grid gap-4 max-w-xs">
-          <div>
-            <Label htmlFor="name" className="block font-semibold text-sm mb-2">
-              Billing Name
-            </Label>
-            <Input id="name" name="name" type="text" />
-          </div>
-          <div>
-            <Label htmlFor="email" className="block font-semibold text-sm mb-2">
-              Billing Email
-            </Label>
-            <Input id="email" name="email" type="email" />
-          </div>
-          <div>
-            <Label htmlFor="value" className="block font-semibold text-sm mb-2">
-              Value
-            </Label>
-            <Input id="value" name="value" type="text" />
-          </div>
-          <div>
-            <Label
-              htmlFor="description"
-              className="block font-semibold text-sm mb-2"
-            >
-              Description
-            </Label>
-            <Textarea id="description" name="description" />
-          </div>
-          <div>
-            <SubmitButton />
-          </div>
-        </form>
       </Container>
     </main>
   );
